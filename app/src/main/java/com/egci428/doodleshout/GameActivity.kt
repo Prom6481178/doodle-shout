36170f1eb8869d1e83dbe8014c9927ac09201a81
LockdownP <Prompanasakulkan@gmail.com>
2025-07-03 20:41:26 +0700

    initial main menu

--- app/src/main/java/com/egci428/doodleshout/GameActivity.kt
+++ app/src/main/java/com/egci428/doodleshout/GameActivity.kt
@@ -1,35 +1,20 @@
 package com.egci428.doodleshout
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_game)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val startButton = findViewById<Button>(R.id.startButton)
-        val exitButton = findViewById<Button>(R.id.exitButton)
-
-        startButton.setOnClickListener {
-            val intent = Intent(this, GameActivity::class.java)
-            startActivity(intent)
-        }
-
-        exitButton.setOnClickListener {
-            finishAffinity()
         }
     }
 }
